2c3140af2c761c1ee74ad04c82cf538e8f6b6587
jean-schneider <84456865+jean-schneider@users.noreply.github.com>
2022-01-14 15:44:29 +0100

    query4

--- Query4/__init__.py
+++ Query4/__init__.py
@@ -1,5 +1,9 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
 import azure.functions as func
 
 
@@ -14,11 +18,38 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = ""
+    try:
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        actors = graph.run("MATCH (p:Name)-[r:ACTED_IN]->(:Title) \
+                                WITH p, COUNT(r) as nbFilms \
+                                WHERE nbFilms > 1 \
+                                RETURN p.primaryName, nbFilms ORDER BY nbFilms DESC, p.primaryName")
+        for actor in actors:
+            dataString += f"CYPHER: {producer['p.primaryName']} acted in {producer['nbFilms']}. \n"
+
+    except:
+        errorMessage = "Erreur de connexion a la base Neo4j"
+        
+    
+    if errorMessage != "":
+        return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
+
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(dataString + nameMessage + " Connexions réussies a Neo4j !")
