932eb17cfb1ee347d39804e04959ec7cc1eedc5b
jean-schneider <84456865+jean-schneider@users.noreply.github.com>
2022-01-15 17:48:35 +0100

    Query 2

--- Query2/__init__.py
+++ Query2/__init__.py
@@ -1,24 +1,53 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
 import azure.functions as func
+import time
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = "Années de naissances les plus représentées parmi les acteurs :"
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            time3 = time.time()
+            cursor.execute(f"SELECT TOP 5 *
+                FROM
+                    (SELECT birthYear, count(primaryName) nombre
+                    FROM tNames 
+                    WHERE birthYear != 0 
+                    GROUP BY birthYear) decompte
+                ORDER BY nombre DESC
+                ", (filteredName,))
+            rows = cursor.fetchall()
+            time4 = time.time()
+            if len(rows) > 0:                   
+                for row in rows:
+                    dataString += f"SQL: {row[0]}\n"
+                dataString += f"Elapsed time : {time4-time3} seconds\n"
+
+    except:
+        errorMessage = "Erreur de connexion a la base SQL"
+    
+    if errorMessage != "":
+        return func.HttpResponse(dataString + errorMessage, status_code=500)
+
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(dataString + " Connexions réussies a Neo4j et SQL!")
